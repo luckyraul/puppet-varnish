06c8e3db9050ccb8a9bdcd06cfa6b3bb0c028b18
Nikita <luckyraul@users.noreply.github.com>
2016-03-01 11:29:22 +0300

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -28,7 +28,7 @@ class varnish::params {
                     $package_version = '4.1'
                 }
                 default: {
-                    fail("Unsupported release: ${::lsbdistcodename}")
+                    fail("Unsupported debian release: ${::lsbdistcodename}")
                 }
           }
       }
